4e0cd8cf9559561602d19b8048027bce49c8bdbf
xschen <xs0040@gmail.com>
2017-12-24 18:47:50 +0800

    minor update

--- keras_image_classifier_train/cnn_bi_classifier_train.py
+++ keras_image_classifier_train/cnn_bi_classifier_train.py
@@ -4,79 +4,83 @@ from keras.layers import Conv2D, MaxPooling2D, Activation, Flatten, Dense, Dropo
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 
-img_width, img_height = 150, 150
 
-train_data_dir = 'bi_classifier_data/training'
-validation_data_dir = 'bi_classifier_data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
-nb_test_samples = 400
-epochs = 50
-batch_size = 16
-WEIGHT_FILE_PATH = 'models/cnn_bi_classifier_weights.h5'
+def main():
+    img_width, img_height = 150, 150
 
-if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
-else:
-    input_shape = (img_width, img_height, 3)
+    train_data_dir = 'bi_classifier_data/training'
+    validation_data_dir = 'bi_classifier_data/validation'
+    nb_train_samples = 2000
+    nb_validation_samples = 800
+    nb_test_samples = 400
+    epochs = 50
+    batch_size = 16
+    WEIGHT_FILE_PATH = 'models/cnn_bi_classifier_weights.h5'
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+    else:
+        input_shape = (img_width, img_height, 3)
 
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
 
-model_json = model.to_json()
-open('models/cnn_bi_classifier_architecture.json','w').write(model_json)
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
-)
+    model_json = model.to_json()
+    open('models/cnn_bi_classifier_architecture.json', 'w').write(model_json)
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+    validation_datagen = ImageDataGenerator(rescale=1. / 255)
 
-train_generator = train_datagen.flow_from_directory(
-    directory=train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary'
-)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-validation_generator = validation_datagen.flow_from_directory(
-    directory=validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary'
-)
+    train_generator = train_datagen.flow_from_directory(
+        directory=train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary'
+    )
 
-checkpoint = ModelCheckpoint(filepath=WEIGHT_FILE_PATH, save_best_only=True)
-model.fit_generator(train_generator,
-                    steps_per_epoch=nb_train_samples // batch_size,
-                    epochs=epochs,
-                    validation_data=validation_generator,
-                    validation_steps=nb_validation_samples // batch_size,
-                    callbacks=[checkpoint])
+    validation_generator = validation_datagen.flow_from_directory(
+        directory=validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary'
+    )
+
+    checkpoint = ModelCheckpoint(filepath=WEIGHT_FILE_PATH, save_best_only=True)
+    model.fit_generator(train_generator,
+                        steps_per_epoch=nb_train_samples // batch_size,
+                        epochs=epochs,
+                        validation_data=validation_generator,
+                        validation_steps=nb_validation_samples // batch_size,
+                        callbacks=[checkpoint])
+
+    model.save_weights(WEIGHT_FILE_PATH, overwrite=True)
 
 
-model.save_weights(WEIGHT_FILE_PATH, overwrite=True)
-
+if __name__ == '__main__':
+    main()
