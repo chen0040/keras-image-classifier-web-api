c83dc465f6faae71848b8a372e4ad4e765241647
xschen <xs0040@gmail.com>
2018-01-17 13:21:42 +0800

    update setup config

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='web',
-    packages=['web'],
+    name='keras_image_classifier',
+    packages=['keras_image_classifier'],
     include_package_data=True,
     install_requires=[
         'flask',
         'keras',
+        'tensorflow',
+        'pillow',
+        'numpy',
+        'h5py',
+        'scikit-learn'
     ],
     setup_requires=[
         'pytest-runner',
